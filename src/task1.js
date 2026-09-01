79d0d9a51a5d415d026d469fa365ef39eac0bb86
Victor Olachea <victor.olachea@gmail.com>
2026-09-01 12:16:58 -0600

    Completar ejercicios de arrays

--- src/task1.js
+++ src/task1.js
@@ -1,8 +1,11 @@
 const arr = []
 
 // Escribe tu codigo por debajo de esta linea
-
-
+for (let i = 1; i <= 20; i++) {
+  arr.push(i);
+  
+}
+console.log("Tarea 1: " + arr);
 
 // Escribe tu codigo por encima de esta linea
 
